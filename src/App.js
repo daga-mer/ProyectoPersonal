64ec677bb872dbc8004c859332ddb97299da1c8c
Daniel Alejandro Gonzalez Almanza <almanzad404@gmail.com>
2021-05-25 14:00:40 -0500

    Se mejoran enlaces de las páginas y agregan informacion de las tecnologias y herramientas que uso

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { CardGroup, Jumbotron } from 'reactstrap';
-import CardS from './Componentes/Cards';
-import HTML5 from './IMGS/HTML5.png';
-import CSS3 from './IMGS/CSS3.svg';
-import JS from './IMGS/JS.png';
-import PHP from './IMGS/PHP.png';
-import REACT from './IMGS/React.png';
-import MySQL from './IMGS/MySQL.png';
-import Gmail from './IMGS/google-gmail.svg';
+import { CardGroup, Jumbotron, Button } from 'reactstrap';
+import LinKedin from './IMGS/linkedin-icon.svg';
+import Angular from './IMGS/angular-icon.svg';
+import Bootstrap from './IMGS/bootstrap.svg';
 import GitHub from './IMGS/github-icon.svg';
+import Gmail from './IMGS/google-gmail.svg';
+import CardS from './Componentes/Cards';
+import Git from './IMGS/git-icon.svg';
+import REACT from './IMGS/React.png';
+import HTML5 from './IMGS/HTML5.png';
+import MySQL from './IMGS/MySQL.png';
+import CSS3 from './IMGS/CSS3.svg';
+import PHP from './IMGS/PHP.png';
+import JS from './IMGS/JS.png';
 import './App.css';
 
 const App = (props) => {
@@ -32,10 +36,11 @@ const App = (props) => {
       <Jumbotron id='Bienvenida' style={{ textAlign: 'center', backgroundColor: 'white' }}>
         <h1 style={{ marginTop: '50px' }}>Bienvenido</h1>
         <p>
-          Mucho gusto mi nombre es Daniel soy un desarrollador web de nivel
-          junior colombiano de 18 años que reside en el municipio de soacha que
-          conoce diversidad de lenguajes y herrramientas con las que puedo llegar
-          a desempeñar mi labor de la mejor manera.
+          Mucho gusto, mi nombre es Daniel soy un joven colombiano desarrollador
+          web de nivel junior de 18 años de edad con 6 meses de experiencia,
+          resido en el municipio de Soacha. Conozco diversidad de lenguajes y
+          herrramientas con las que puedo llegar a desempeñar mi labor de la mejor
+          manera.
         </p>
       </Jumbotron>
 
@@ -47,7 +52,7 @@ const App = (props) => {
       </div>
 
       <h1 style={{ textAlign: 'center', backgroundColor: 'black', margin: 0, color: 'white' }}>
-        Tecnologias que manejo
+        Tecnologias y herramientas que manejo
         </h1>
 
       <CardGroup style={{ backgroundColor: 'black' }}>
@@ -67,10 +72,22 @@ const App = (props) => {
           Img={PHP}
         />
         <CardS
-          Img={REACT}
+          Img={MySQL}
         />
         <CardS
-          Img={MySQL}
+          Img={REACT}
+        />
+      </CardGroup>
+
+      <CardGroup style={{ backgroundColor: 'black' }}>
+        <CardS
+          Img={Angular}
+        />
+        <CardS
+          Img={Bootstrap}
+        />
+        <CardS
+          Img={Git}
         />
       </CardGroup>
 
@@ -81,19 +98,35 @@ const App = (props) => {
         </svg>
       </div>
 
+      <div style={{ textAlign: 'center' }}>
+        <a href='https://drive.google.com/file/d/1CTI-4c47ImBjL2y2gJ90R2owXIG-pkA5/view?usp=sharing' rel="noreferrer" target="_blank">
+          <Button color="primary">
+            Descarga mi CV
+          </Button>
+        </a>
+      </div>
+
       <div style={{ height: '150px', overflow: 'hidden' }} >
         <svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{ height: '100%', width: '100%' }}>
           <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style={{ stroke: 'none', fill: '#000' }}>
           </path>
         </svg>
       </div>
+
       <footer id='Contacto'>
+
         <h4>Medios de comunicación</h4>
+
         <p style={{ margin: '0%', paddingBottom: '2%' }}>
+
           <a href='https://github.com/daga-mer' rel="noreferrer" target="_blank">
-            <img src={GitHub} alt="" width='25px' height='25px' style={{ backgroundColor: "white", borderRadius: "100px" }} /> Click Aqui</a>
-          <a href="mailto:almanzad404@gmail.com">
-            <img src={Gmail} alt="" width='25px' height='25px' style={{ marginLeft: '10px' }} />  almanzad404@gmail.com</a>
+            <img src={GitHub} alt="" width='25px' height='25px' style={{ backgroundColor: "white", borderRadius: "100px" }} /> GitHub</a>
+
+          <a href="mailto:almanzad404@gmail.com" rel="noreferrer" target="_blank">
+            <img src={Gmail} alt="" width='25px' height='25px' style={{ marginLeft: '10px' }} />  Correo electronico</a>
+
+          <a href="http://www.linkedin.com/in/daniel-alejandro-gonzalez-almanza-66141720a" rel="noreferrer" target="_blank">
+            <img src={LinKedin} alt="" width='25px' height='25px' style={{ marginLeft: '10px' }} />  LinKedin</a>
         </p>
       </footer>
     </div >
